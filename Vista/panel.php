ffbe65c695aff4564e87ef3be12c10a5b0f9580b
Andrew21plus <118701681+Andrew21plus@users.noreply.github.com>
2025-01-13 14:14:42 -0500

    Update panel.php

--- Vista/panel.php
+++ Vista/panel.php
@@ -8,7 +8,8 @@ date_default_timezone_set($user_timezone);
 // Obtener la zona horaria actualmente configurada
 $current_timezone = date_default_timezone_get();
 
-
+// Imprimir la zona horaria
+echo "<script>console.log('La zona horaria actual es: " . $current_timezone . "');</script>";
 
 header('Cache-Control: no-cache, no-store, must-revalidate');
 header('Pragma: no-cache');
