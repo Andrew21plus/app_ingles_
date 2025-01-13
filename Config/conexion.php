c50bb28bc2cc6b43c401f5971c32cb9fd87a7b85
Andrew21plus <118701681+Andrew21plus@users.noreply.github.com>
2025-01-13 13:39:28 -0500

    Update conexion.php

--- Config/conexion.php
+++ Config/conexion.php
@@ -2,7 +2,7 @@
 $host = 'free.clusters.zeabur.com';
 $user = 'root';
 $pass = '1J7ETDQZ2B6o5xaq3IldzVHL0m8ts4C9';
-$db = 'app_ingles';
+$db = 'zeabur';
 
 
 $con = mysqli_connect($host, $user, $pass, $db);
