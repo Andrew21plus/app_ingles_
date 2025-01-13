4a3b07418a115b5f409330dac7a5f7e644cba4ee
Andrew21plus <118701681+Andrew21plus@users.noreply.github.com>
2025-01-13 14:22:23 -0500

    Update panel.php

--- Vista/panel.php
+++ Vista/panel.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html>
+<html lang="en">
 <?php
 include_once '../Modelo/zona_horaria.php';
 include_once '../Config/conexion.php';
@@ -100,8 +102,6 @@ if (!isset($_SESSION['code_executed'])) {
     }
  
     ?>
-<!DOCTYPE html>
-<html lang="en">
       <script>
         document.addEventListener('DOMContentLoaded', function() {
           <?php
